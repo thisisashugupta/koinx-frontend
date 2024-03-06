58200bae29761a4ab4a3addc8b374746dd849b4c
Ashu Gupta <ashugpt1812@gmail.com>
2024-03-07 02:56:06 +0530

    permanent redirect to /coins/bitcoin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import Image from "next/image";
-import Locator from "@/components/Locator";
+import { permanentRedirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="min-h-screen min-w-screen flex flex-col items-left bg-gray-100">
-      <Locator />
-    </main>
-  );
+  permanentRedirect("/coins/bitcoin");
 }

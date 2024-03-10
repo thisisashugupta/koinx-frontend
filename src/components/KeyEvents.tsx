c13a7270d4db3ded7b2c06de6e02889a9ca7bab8
Ashu Gupta <ashugpt1812@gmail.com>
2024-03-10 19:38:11 +0530

    changed key events styling

--- src/components/KeyEvents.tsx
+++ src/components/KeyEvents.tsx
@@ -1,3 +1,4 @@
+// @/componenets/KeyEvents.tsx
 import * as React from "react"
 
 import { Card, CardContent } from "@/components/ui/card"
@@ -10,12 +11,15 @@ import {
 } from "@/components/ui/carousel"
 
 export default function KeyEvents() {
+
+  const colors = ['bg-red', 'bg-yellow', 'bg-green', 'bg-indigo', 'bg-fuchsia'];
+
   return (
     <Carousel
       opts={{
         align: "start",
       }}
-      className="w-full max-w-sm"
+      className="w-full max-w-sm md:max-w-full"
     >
       <CarouselContent>
         {/* {Array.from({ length: 5 }).map((_, index) => ( */}
@@ -23,10 +27,10 @@ export default function KeyEvents() {
           // key={index} 
           className="basis-4/5 md:basis-1/2 lg:basis-1/3">
             <div>
-              <Card className="m-1 bg-blue-300/20">
+              <Card className={`m-1 bg-red-300/20`}>
                 <CardContent className="flex items- justify-between p-3 pb-6 gap-3">
                   <div>
-                    <div className="w-8 h-8 rounded-full bg-blue-500"></div>
+                    <div className={`w-8 h-8 rounded-full bg-red-500`}></div>
                   </div>
                   <div className="w-full">
                     <div className="text-xs leading-tight font-medium">Lorem ipsum dolor sit amet consectetur. Dui vel quis dignissim </div>
@@ -36,10 +40,9 @@ export default function KeyEvents() {
               </Card>
             </div>
           </CarouselItem>
+          {/* ))} */}
 
-          <CarouselItem 
-          // key={index} 
-          className="basis-4/5 md:basis-1/2 lg:basis-1/3">
+          <CarouselItem className="basis-4/5 md:basis-1/2 lg:basis-1/3">
           <div>
             <Card className="m-1 bg-green-300/20">
               <CardContent className="flex items- justify-between p-3 pb-6 gap-3">
@@ -54,7 +57,55 @@ export default function KeyEvents() {
             </Card>
           </div>
           </CarouselItem>
-        {/* ))} */}
+
+          <CarouselItem className="basis-4/5 md:basis-1/2 lg:basis-1/3">
+            <div>
+              <Card className="m-1 bg-yellow-300/20">
+                <CardContent className="flex items- justify-between p-3 pb-6 gap-3">
+                  <div>
+                    <div className="w-8 h-8 rounded-full bg-yellow-500"></div>
+                  </div>
+                  <div className="w-full">
+                    <div className="text-xs leading-tight font-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit</div>
+                    <div className="text-2xs leading-snug text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum asperiores fuga vitae similique ullam officiis praesentium architecto. Dolore odio </div>
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
+            </CarouselItem>
+                
+            <CarouselItem className="basis-4/5 md:basis-1/2 lg:basis-1/3">
+            <div>
+              <Card className="m-1 bg-indigo-300/20">
+                <CardContent className="flex items- justify-between p-3 pb-6 gap-3">
+                  <div>
+                    <div className="w-8 h-8 rounded-full bg-indigo-500"></div>
+                  </div>
+                  <div className="w-full">
+                    <div className="text-xs leading-tight font-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit</div>
+                    <div className="text-2xs leading-snug text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum asperiores fuga vitae similique ullam officiis praesentium architecto. Dolore odio </div>
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
+            </CarouselItem>
+                
+            <CarouselItem className="basis-4/5 md:basis-1/2 lg:basis-1/3">
+            <div>
+              <Card className="m-1 bg-fuchsia-300/20">
+                <CardContent className="flex items- justify-between p-3 pb-6 gap-3">
+                  <div>
+                    <div className="w-8 h-8 rounded-full bg-fuchsia-500"></div>
+                  </div>
+                  <div className="w-full">
+                    <div className="text-xs leading-tight font-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit</div>
+                    <div className="text-2xs leading-snug text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum asperiores fuga vitae similique ullam officiis praesentium architecto. Dolore odio </div>
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
+            </CarouselItem>
+        
       </CarouselContent>
     </Carousel>
   )
